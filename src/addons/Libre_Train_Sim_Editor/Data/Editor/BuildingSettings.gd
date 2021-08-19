44f04764e12bc4c1b79776ff426dd5dc622e7baa
Jean28518 <jf_v@gmx.de>
2021-08-19 23:15:54 +0200

    Implementing Material Assign for Objects

---
+++ src/addons/Libre_Train_Sim_Editor/Data/Editor/BuildingSettings.gd
@@ -0,0 +1,77 @@
+extends VBoxContainer
+
+
+var current_material_index = 0
+var current_mesh = null
+var material_count = 0
+
+var requested_content_selector = false
+
+onready var editor = find_parent("Editor")
+onready var content_selector = editor.get_node("EditorHUD/Content_Selector")
+
+func _input(event):
+	if not is_instance_valid(current_mesh):
+		hide()
+
+
+func set_mesh(mesh : MeshInstance):
+	current_mesh = mesh
+	clear_materials_list()
+	material_count = mesh.get_surface_material_count()
+	for i in range(material_count):
+		var new_child = get_node("Material-1").duplicate()
+		new_child.name = "Material" + String(i)
+		var current_material = current_mesh.get_surface_material(i)
+		if current_material != null:
+			new_child.get_node("LineEdit").text = current_material.resource_path
+		new_child.get_node("Label").text += String(i)
+		add_child(new_child)
+		new_child.show()
+	
+	## Add Update Button:
+	var button = Button.new()
+	button.name = "Button"
+	button.text = "Update"
+	button.connect("pressed", self, "set_current_config_to_mesh")
+	add_child(button)
+	
+	show()
+
+func set_current_config_to_mesh():
+	if not is_instance_valid(current_mesh):
+		return
+	var counter = 0
+	for child in get_children():
+		if child.name == "Material-1" or child is Button:
+			continue
+		var material_path = child.get_node("LineEdit").text
+		if ResourceLoader.exists(material_path):
+			current_mesh.set_surface_material(counter, load(material_path))
+		counter += 1
+			
+		
+	
+
+func clear_materials_list():
+	for child in get_children():
+		if child.name != "Material-1":
+			child.queue_free()
+
+func pick_pressed(material_index):
+	current_material_index = material_index
+	content_selector.set_type(content_selector.MATERIALS)
+	content_selector.show()
+	requested_content_selector = true
+
+
+func _on_Content_Selector_resource_selected(complete_path):
+	if not requested_content_selector:
+		return
+	requested_content_selector = false
+	if complete_path == "":
+		return
+	if not is_instance_valid(current_mesh):
+		return
+	get_child(current_material_index + 1).get_node("LineEdit").text = complete_path
+	set_current_config_to_mesh()
--- src/addons/Libre_Train_Sim_Editor/Data/Scripts/Editor.gd
+++ src/addons/Libre_Train_Sim_Editor/Data/Scripts/Editor.gd
@@ -96,6 +96,8 @@ func provide_settings_for_selected_object():
 	if selected_object_type == "Rail":
 		$EditorHUD/Settings/TabContainer/RailAttachments.update_selected_rail(selected_object)
 		$EditorHUD/Settings/TabContainer/RailBuilder.update_selected_rail(selected_object)
+	if selected_object_type == "Building":
+		$EditorHUD/Settings/TabContainer/BuildingSettings.set_mesh(selected_object)
 
 ## Should be used, if world is loaded into scene.
 func load_world():

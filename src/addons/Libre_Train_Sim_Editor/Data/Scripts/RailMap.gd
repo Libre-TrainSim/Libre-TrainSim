b70d0d4584aabeb3ecdfefe5fcb3f9dfa25c3022
Jean3219 <39700889+Jean28518@users.noreply.github.com>
2021-08-27 20:33:01 +0200

    Update RailMap.gd

--- src/addons/Libre_Train_Sim_Editor/Data/Scripts/RailMap.gd
+++ src/addons/Libre_Train_Sim_Editor/Data/Scripts/RailMap.gd
@@ -1,6 +1,6 @@
 extends Viewport
 
-const LINE_POINT_INTERVAL = 10 # 1 line point for every 10 meters of track
+const LINE_POINT_INTERVAL = 25 # 1 line point for every 25 meters of track
 
 onready var train_world = find_parent("World")
 onready var camera = $Camera2D
@@ -266,13 +266,23 @@ func build_rail(rail) -> Array:
 	else:
 		length = rail.length
 	
-	var point_count = int(length / LINE_POINT_INTERVAL) + 1
-	# add point count many points along track
-	for i in range(0,point_count):
-		var rail_transform = rail.get_global_transform_at_rail_distance(i*LINE_POINT_INTERVAL)
+	# detailed rail for curves
+	if rail.radius != 0:
+		var point_count = int(length / LINE_POINT_INTERVAL) + 1
+		# add point count many points along track
+		for i in range(0,point_count):
+			var rail_transform = rail.get_global_transform_at_rail_distance(i*LINE_POINT_INTERVAL)
+			points.append(Vector2(rail_transform.origin.x, rail_transform.origin.z))
+		# add end point
+		var rail_transform = rail.get_global_transform_at_rail_distance(rail.length)
 		points.append(Vector2(rail_transform.origin.x, rail_transform.origin.z))
-	# add end point
-	var rail_transform = rail.get_global_transform_at_rail_distance(rail.length)
-	points.append(Vector2(rail_transform.origin.x, rail_transform.origin.z))
-	
+	# only 2 points for straight rails
+	else: 
+		# Start Point
+		var rail_transform = rail.get_global_transform_at_rail_distance(0)
+		points.append(Vector2(rail_transform.origin.x, rail_transform.origin.z))
+		# End Point
+		rail_transform = rail.get_global_transform_at_rail_distance(rail.length)
+		points.append(Vector2(rail_transform.origin.x, rail_transform.origin.z))
+		
 	return points

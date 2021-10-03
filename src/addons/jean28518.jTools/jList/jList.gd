1e3ea85fc21628a237d4ed5ebac503cda73ea21d
Johannes Witt <johawitt@outlook.de>
2021-10-03 18:37:03 +0200

    Fix memory leaks

--- src/addons/jean28518.jTools/jList/jList.gd
+++ src/addons/jean28518.jTools/jList/jList.gd
@@ -6,13 +6,13 @@ signal user_removed_entries(entry_names) # array of strings
 signal user_renamed_entry(old_name, new_name) # string
 signal user_duplicated_entries(source_entry_names, duplicated_entry_names) # arrays of strings
 signal user_copied_entries(entry_names)
-signal user_pasted_entries(source_entry_names, source_jList_id, pasted_entry_names) 
+signal user_pasted_entries(source_entry_names, source_jList_id, pasted_entry_names)
 signal user_pressed_save(data) # array of strings (equal to entry_names)
 signal user_selected_entry(entry_name) # string
 signal user_pressed_action(entry_names) # array of strings (equal to entry_names)
 
 export (String) var _id = "_random"
-var id 
+var id
 export (String) var entry_duplicate_text = "_duplicate"
 
 export (bool) var only_unique_entries_allowed = true
@@ -22,9 +22,9 @@ export (bool) var enable_add_button = true
 export (bool) var enable_remove_button = true
 export (bool) var enable_rename_button = false
 export (bool) var enable_duplicate_button = false
-export (bool) var enable_copy_button = false 
-export (bool) var enable_paste_button = false 
-export (bool) var enable_save_button = false 
+export (bool) var enable_copy_button = false
+export (bool) var enable_paste_button = false
+export (bool) var enable_save_button = false
 export (bool) var enable_action_button = false
 
 export (String) var add_button_text = "Add"
@@ -67,7 +67,7 @@ func remove_entry(entry_name : String):
 
 func has_entry(entry_name : String):
 	return -1 != get_entry_id(entry_name)
-	
+
 func select_entry(entry_name : String):
 	if not has_entry(entry_name):
 		print_debug("jList " + name + ": Entry " + entry_name + " not found. Dont selecting anything...")
@@ -82,13 +82,13 @@ func revoke_last_user_action(message : String = ""):
 		return
 	set_data(undo_buffer)
 	undo_buffer = null
-		
+
 	if message != "":
 		$PopupDialog/Label.text = message
 	else:
 		$PopupDialog/Label.text = "This action is not allowed!"
 	$PopupDialog.popup_centered_minsize()
-	
+
 ## Internal Code ###############################################################
 var item_list
 
@@ -100,9 +100,10 @@ func _ready():
 		$VBoxContainer/ItemList.select_mode = ItemList.SELECT_MULTI
 	else:
 		$VBoxContainer/ItemList.select_mode = ItemList.SELECT_SINGLE
-	
-func _process(delta):
-	if $VBoxContainer/HBoxContainer/LineEdit.has_focus() and enable_add_button and  Input.is_action_just_pressed("jList_enter"):
+
+func _unhandled_key_input(_event: InputEventKey) -> void:
+	if $VBoxContainer/HBoxContainer/LineEdit.has_focus() and enable_add_button \
+			and Input.is_action_just_pressed("ui_enter"):
 		_on_Add_pressed()
 
 func is_entry_name_unique(entry : String):
@@ -133,7 +134,7 @@ func rename_entry_id(entry_id : int, new_entry_name : String):
 
 func duplicate_entry_id(entry_id : int):
 	return add_entry(item_list.get_item_text(entry_id))
-	
+
 func remove_entry_id(entry_id : int):
 	if entry_id >= get_size():
 		print_debug("jList " + name + ": remove_entry_id(): entry_id out of bounds! Skipping...")
@@ -149,7 +150,7 @@ func update_visible_buttons(newvar):
 	$VBoxContainer/HBoxContainer/Paste.visible = enable_paste_button
 	$VBoxContainer/HBoxContainer/Save.visible = enable_save_button
 	$VBoxContainer/HBoxContainer/Action.visible = enable_action_button
-	
+
 	$VBoxContainer/HBoxContainer/Add.text = TranslationServer.translate(add_button_text)
 	$VBoxContainer/HBoxContainer/Remove.text = TranslationServer.translate(remove_button_text)
 	$VBoxContainer/HBoxContainer/Rename.text = TranslationServer.translate(rename_button_text)
@@ -158,10 +159,10 @@ func update_visible_buttons(newvar):
 	$VBoxContainer/HBoxContainer/Paste.text = TranslationServer.translate(paste_button_text)
 	$VBoxContainer/HBoxContainer/Save.text = TranslationServer.translate(save_button_text)
 	$VBoxContainer/HBoxContainer/Action.text = TranslationServer.translate(action_button_text)
-	
+
 	_update_fonts()
 	update = false
-	
+
 func _update_fonts():
 	if custom_font_path == "":
 		return
@@ -177,7 +178,7 @@ func _update_fonts():
 	$VBoxContainer/HBoxContainer/Paste.add_font_override("font", font)
 	$VBoxContainer/HBoxContainer/Save.add_font_override("font", font)
 	$VBoxContainer/HBoxContainer/Action.add_font_override("font", font)
-	
+
 	$VBoxContainer/ItemList.add_font_override("font", font)
 	$PopupDialog/Label.add_font_override("font", font)
 	$PopupDialog/Okay.add_font_override("font", font)
@@ -191,10 +192,7 @@ func _enter_tree():
 			id = String(randi())
 		else:
 			id = _id
-		jListManager.register_jList(self)
 
-func _exit_tree():
-	jListManager.deregister_jList(self)
 
 func _on_Add_pressed():
 	if $VBoxContainer/HBoxContainer/LineEdit.text == "":
@@ -212,7 +210,7 @@ func _on_Remove_pressed():
 	while item_list.get_selected_items().size() != 0:
 		removed_entries.append(item_list.get_item_text(item_list.get_selected_items()[0]))
 		remove_entry_id(item_list.get_selected_items()[0])
-	
+
 	emit_signal("user_removed_entries", removed_entries)
 
 func _on_Rename_pressed():
@@ -240,7 +238,7 @@ func _on_Duplicate_pressed():
 	for entry_id in source_entry_ids:
 		source_entry_names.append(item_list.get_item_text(entry_id))
 		duplicated_entry_names.append(duplicate_entry_id(entry_id))
-	
+
 	emit_signal("user_duplicated_entries", source_entry_names, duplicated_entry_names)
 
 func _on_Copy_pressed(): # stores the current entry_names into the global buffer
@@ -251,18 +249,18 @@ func _on_Copy_pressed(): # stores the current entry_names into the global buffer
 	var source_entry_ids = item_list.get_selected_items()
 	for entry_id in source_entry_ids:
 		source_entry_names.append(item_list.get_item_text(entry_id))
-	jListManager.set_buffer(self, source_entry_names)
+	OS.clipboard = source_entry_names
 	emit_signal("user_copied_entries", source_entry_names)
 
 func _on_Paste_pressed(): # Adds entry_names from global buffer into jList.
-	var source_entry_names = jListManager.get_buffer()
+	var source_entry_names = OS.clipboard
 	if source_entry_names == null:
 		return
 	undo_buffer = get_data()
 	var pasted_entry_names = []
 	for source_entry in source_entry_names:
 		pasted_entry_names.append(add_entry(source_entry))
-	emit_signal("user_pasted_entries", source_entry_names, jListManager.get_buffer_source_jList_id(), pasted_entry_names)
+	emit_signal("user_pasted_entries", source_entry_names, id, pasted_entry_names)
 
 
 func _on_Save_pressed():
@@ -276,7 +274,7 @@ func _on_Action_pressed():
 	for entry_id in source_entry_ids:
 		source_entry_names.append(item_list.get_item_text(entry_id))
 	emit_signal("user_pressed_action", source_entry_names)
-	
+
 
 func _on_ItemList_item_activated(index):
 	$VBoxContainer/HBoxContainer/LineEdit.text = item_list.get_item_text(index)

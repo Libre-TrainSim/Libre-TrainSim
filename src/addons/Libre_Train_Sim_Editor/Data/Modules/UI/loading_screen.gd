463fcdae00797a33df4c91e7ae5158eb4e71433e
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2021-10-16 13:20:18 +0200

    add LoadingScreen.load_main_menu()

--- src/addons/Libre_Train_Sim_Editor/Data/Modules/UI/loading_screen.gd
+++ src/addons/Libre_Train_Sim_Editor/Data/Modules/UI/loading_screen.gd
@@ -15,6 +15,21 @@ func _ready() -> void:
 	set_process(false)
 
 
+func load_main_menu():
+	loader = ResourceLoader.load_interactive("res://addons/Libre_Train_Sim_Editor/Data/Modules/main_menu.tscn")
+	get_tree().current_scene.queue_free()
+	get_tree().current_scene = self
+	set_process(true)
+	get_tree().paused = false
+	jAudioManager.clear_all_sounds()
+	jEssentials.remove_all_pending_delayed_calls()
+	$ProgressBar/Bar.max_value = loader.get_stage_count()
+	$ProgressBar/Bar.value = 0
+	$ProgressBar/Description.lines_skipped = 0
+	$Screenshot.texture = load("res://screenshot.png") as Texture
+	show()
+
+
 func load_world(world: String, scenario: String, train: String, bg_img: Texture) -> void:
 	loader = ResourceLoader.load_interactive(world)
 	get_tree().current_scene.queue_free()

5795ea83d77d08f9f405be6515600f44d0ce9ec5
Johannes Witt <johawitt@outlook.de>
2022-01-23 12:36:17 +0100

    Fix translations not getting applied (#320)

---
+++ src/Data/UI/translated_rich_rext_label.gd
@@ -0,0 +1,25 @@
+class_name TranslatedRichTextLabel
+extends RichTextLabel
+
+
+onready var translation_id := text
+
+
+func _ready() -> void:
+	update_text()
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_TRANSLATION_CHANGED:
+		update_text()
+
+
+func set_text(text: String) -> void:
+	.set_text(tr(text))
+
+
+func update_text() -> void:
+	if bbcode_enabled:
+		bbcode_text = tr(translation_id)
+	else:
+		text = tr(translation_id)

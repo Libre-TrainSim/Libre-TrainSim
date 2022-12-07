61da8c249f6cb16ff58d4d59a199defd096f8675
Johannes Witt <johawitt@outlook.de>
2022-12-07 23:32:39 +0100

    Revert "Save user settings in editor"
    This reverts commit 8162a124e9da4d0f42f709f280e0d6bc98126251.

--- src/Data/Scripts/Singletons/Root.gd
+++ src/Data/Scripts/Singletons/Root.gd
@@ -150,6 +150,10 @@ func get_subfolders_of(directory_path: String) -> Array:
 	return folder_names
 
 
+func set_fullscreen(value: bool) -> void:
+	OS.window_fullscreen = value
+
+
 func set_low_resolution(value: bool) -> void:
 	if value:
 		if ProjectSettings.get_setting("display/window/stretch/mode") == "viewport":
@@ -161,7 +165,7 @@ func set_low_resolution(value: bool) -> void:
 	else:
 		ProjectSettings.set_setting("display/window/stretch/mode", "disabled")
 		ProjectSettings.set_setting("display/window/stretch/aspect", "ignore")
-		ProjectSettings.set_setting("display/window/size/width", "1024")
+		ProjectSettings.set_setting("display/window/size/width", "800")
 		ProjectSettings.set_setting("display/window/size/height", "600")
 
 

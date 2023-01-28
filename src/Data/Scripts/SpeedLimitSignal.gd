f4c6d536afbf99b24b977bd8983af1b9c4e88f47
Johannes Witt <johawitt@outlook.de>
2023-01-28 22:33:10 +0100

    Fix speed limits not updating their sign

--- src/Data/Scripts/SpeedLimitSignal.gd
+++ src/Data/Scripts/SpeedLimitSignal.gd
@@ -2,13 +2,13 @@ class_name SpeedLimit
 extends RailLogic
 
 
-export (float) var speed: float
+export (float) var speed: float setget set_speed
 
 
 func set_speed(val: float) -> void:
-	if not is_inside_tree():
-		return
 	speed = val
+	if not is_inside_tree():
+		yield(self, "ready")
 	$Viewport/Node2D/Label.text = str(int(val/10))
 
 

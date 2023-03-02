469c3e824017e02ffa465e149ece93a170fff5bc
nalquas <nalquas.dev@gmail.com>
2023-03-02 13:15:54 +0100

    Use Label3D for SpeedLimit signs Lf6, Lf6_small, Zs3v, Zs3v_small

--- src/Data/Scripts/SpeedLimitWarnSignal.gd
+++ src/Data/Scripts/SpeedLimitWarnSignal.gd
@@ -12,6 +12,4 @@ func _get_type() -> String:
 func _ready() -> void:
 	if not Root.Editor:
 		$SelectCollider.queue_free()
-	$Mesh.set_surface_material(2, $Mesh.get_surface_material(2).duplicate(true))
-	$Mesh.get_surface_material(2).albedo_texture = $Viewport.get_texture()
 	set_to_rail()

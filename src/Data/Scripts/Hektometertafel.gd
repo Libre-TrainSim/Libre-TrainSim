4e090ca64f343e3b2d6f4c6db8a128e7036f4ee8
nalquas <nalquas.dev@gmail.com>
2023-03-02 12:59:13 +0100

    Use Label3D in Hektometertafel.tscn

--- src/Data/Scripts/Hektometertafel.gd
+++ src/Data/Scripts/Hektometertafel.gd
@@ -4,9 +4,7 @@ export (int) var distance: int = 0
 
 func _ready() -> void:
 	if not Engine.editor_hint:
-		$Mesh.set_surface_material(1, $Mesh.get_surface_material(1).duplicate(true))
-		$Mesh.get_surface_material(1).albedo_texture = $Viewport.get_texture()
 		var km = int(distance / 1000)
 		var m = int((distance - km*1000) / 100)
-		$Viewport/Control/VBoxContainer/km.text = str(km)
-		$Viewport/Control/VBoxContainer/m.text = str(m)
+		$LabelTop.text = str(km)
+		$LabelBottom.text = str(m)

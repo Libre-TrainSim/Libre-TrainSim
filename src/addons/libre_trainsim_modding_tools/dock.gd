219d510faf674d636499c62ade254601312ef410
Johannes Witt <johawitt@outlook.de>
2023-03-24 23:13:49 +0100

    Fix modding tools not working due to wrong margins

--- src/addons/libre_trainsim_modding_tools/dock.gd
+++ src/addons/libre_trainsim_modding_tools/dock.gd
@@ -23,8 +23,6 @@ func _on_open_addons_dir_pressed() -> void:
 
 func _on_export_mod_pressed() -> void:
 	dir_select_dialog = FileDialog.new()
-	dir_select_dialog.rect_min_size = Vector2(500, 300)
-	dir_select_dialog.rect_size = Vector2(500, 300)
 	dir_select_dialog.resizable = true
 	dir_select_dialog.window_title = "Select Mod to Export"
 	dir_select_dialog.mode = FileDialog.MODE_OPEN_DIR
@@ -32,7 +30,7 @@ func _on_export_mod_pressed() -> void:
 	dir_select_dialog.current_dir = "res://Mods"
 	dir_select_dialog.connect("dir_selected", self, "_on_export_dir_selected")
 	base.add_child(dir_select_dialog)
-	dir_select_dialog.popup_centered()
+	dir_select_dialog.popup_centered_ratio()
 
 
 func _on_export_dir_selected(dir: String) -> void:
